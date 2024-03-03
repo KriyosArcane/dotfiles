c8aa5f68700cc099062f430b5408b7c2dbbccdd3
Fagan Afandiyev <kriyosthearcane@gmail.com>
2024-03-02 19:27:48 -0500

    commit

---
+++ scripts/zsh_config.sh
@@ -0,0 +1,5 @@
+# Load necessary shell configurations
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# Other configurations as needed
